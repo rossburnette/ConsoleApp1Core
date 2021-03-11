3d157f07dbaaf5dd0695edafa27d5a15eb265a9c
Ross Burnette <reb1981@outlook.com>
2021-03-11 14:14:19 -0600

    Add project files.

---
+++ ConsoleApp1Core/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Dynamic;
+
+namespace ConsoleApp1Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic customer = new ExpandoObject();
+            customer.Name = "Ross";
+
+            Console.WriteLine(customer.Name);
+            Console.WriteLine(customer.Name.GetType());
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
